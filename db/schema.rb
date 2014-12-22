50148aaaa82512a29bb302419d1b2ad9d4bb894f
Nina Stache <nina.stache@absolventa.de>
2014-12-22 15:22:58 +0100

    migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141222122127) do
+ActiveRecord::Schema.define(version: 20141222141447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.string "image"
+  end
 
   create_table "ideas", force: :cascade do |t|
     t.string   "title"
@@ -22,6 +27,8 @@ ActiveRecord::Schema.define(version: 20141222122127) do
     t.string   "location"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "category_id"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
