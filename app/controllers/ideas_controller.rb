419e2d099f96eef398ac42fe26e231a40f82b3d2
Nina Stache <nina.stache@absolventa.de>
2014-12-22 11:43:28 +0100

    add new, create and show action

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -3,4 +3,23 @@ class IdeasController < ApplicationController
   def index
   end
 
+  def new
+    @idea = Idea.new
+  end
+
+  def create
+    @idea = Idea.create(permitted_params)
+    redirect_to @idea
+  end
+
+  def show
+    @idea = Idea.find(params[:id])
+  end
+
+  protected
+
+  def permitted_params
+    params.require(:idea).permit(:title, :description, :location)
+  end
+
 end
